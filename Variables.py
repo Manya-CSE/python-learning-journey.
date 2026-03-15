6bea96b8dfd041a2cb10161ef76cf19a7faa07d9
Manya H S <230591842+Manya-CSE@users.noreply.github.com>
2026-03-15 11:10:15 +0530

    Add Variables.py with examples of data types

---
+++ Variables.py
@@ -0,0 +1,29 @@
+#strings
+first_name="john"
+last_name="doe"
+email="unknown@email.com"
+print(f"my name is {first_name} {last_name}")
+print(f"my email is {email}") 
+
+#integers
+age=25
+quantity=5
+num_students=10
+print(f"I'm {age} years old ")
+print(f"my brother brought {quantity} cone ice-cream")
+print(f"there are only {num_students} students are present in the class")
+
+#floats
+price=499.9
+weight=1.5
+temperature=45.7
+print(f"the price of the dress is {price} Rs")
+print(f"the weight of the apples is {weight} kg")
+print(f"In Manglore the temperature is too high  of about {temperature} degree celcius")
+
+#Booleans
+is_weekend=True
+if is_weekend:
+    print("it's a weekend, lets Enjoy")
+else:
+    print("It's not a weekend yet,keep working")
