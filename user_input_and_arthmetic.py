54b3372076374c6622e23d1dc648573b68871181
Manya H S <230591842+Manya-CSE@users.noreply.github.com>
2026-03-20 12:37:01 +0530

    Create user_input_and_arthmetic.py

---
+++ user_input_and_arthmetic.py
@@ -0,0 +1,28 @@
+#exercise1(area of rectangle)
+length=int(input("enter the length of rectangle:"))
+breadth=float(input("enter the breadth of rectangle:"))
+area=length*breadth
+print(f" The area of the rectangle:{area}")
+
+#Friend's birthday party
+items=input("enter the items that you wanna eat:")
+price=float(input("enter the price of the items :"))
+quantity=int(input("enter the quantity of the items :"))
+total=price*quantity
+print(total)
+
+#madlib game (b party)
+color=input("enter the color:")
+place=input("enter the place:")
+resort=input("enter the resort name:")
+print(f"My sister's bacheloreette party is going to helde at {place} in the beach side {resort}and the theme of the dress is {color}") 
+print("I'm soo exited to enjoy with ny cousins!!") 
+
+#Arithemetic operators
+friends=int(input("enter your  how many friends:"))
+friends+=1
+friends-=1
+friends*=1
+friends**2
+friends/2
+print(friends)
